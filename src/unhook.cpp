b43ce6026406a24a1e73f0b791e3beade6d681ae
Paul Beckingham <paul@beckingham.net>
2016-01-24 16:08:45 -0500

    unhook: Documented algorithm

--- src/unhook.cpp
+++ src/unhook.cpp
@@ -58,7 +58,10 @@ void handleUnhook (
   if (location == "")
     throw std::string ("Queue '" + name + "' not configured.");
 
-  std::cout << "# unhook unimplemnted.\n";
+  // TODO Assert entry exists.
+  // TODO Remove configuration.
+
+  std::cout << "# unhook unimplemented.\n";
 }
 
 ////////////////////////////////////////////////////////////////////////////////
